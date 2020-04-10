0832a8bd4e4ac951aa46cd9efdb94397f970808a
Sunil Thatal <sunilthatal007@gmail.com>
2020-04-10 22:01:49 +0530

    upating page with sho page, collection preveiew and collection item

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+
 function App() {
 	return (
 		<div>
 			<Router>
 				<Switch>
 					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
 				</Switch>
 			</Router>
 		</div>
