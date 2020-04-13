f806c846d5822c2ee5b72848b9b8c445f72cee5b
Sunil Thatal <sunilthatal007@gmail.com>
2020-04-13 10:02:40 +0530

    Rouuter moved to index js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
@@ -20,7 +20,7 @@ class App extends React.Component {
 	componentDidMount() {
 		console.log(process.env);
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-			if(userAuth){
+			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.onSnapshot(snapShot => {
@@ -31,7 +31,7 @@ class App extends React.Component {
 						}
 					})
 				});
-			}else{
+			} else {
 				this.setState({
 					currentUser: userAuth
 				});
@@ -46,14 +46,12 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Router>
-					<Header currentUser={this.state.currentUser}/>
-					<Switch>
-						<Route exact path="/" component={HomePage} />
-						<Route path="/shop" component={ShopPage} />
-						<Route exact path="/signin" component={SignInAndSignUp} />
-					</Switch>
-				</Router>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route exact path="/signin" component={SignInAndSignUp} />
+				</Switch>
 			</div>
 		);
 	}
