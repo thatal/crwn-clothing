c90f463cb94db4c1282aa950049ee4d6da1b67a8
Sunil Thatal <sunilthatal007@gmail.com>
2020-04-14 20:42:27 +0530

    condition on sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
@@ -14,6 +14,7 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
+		console.log(this.props);
 		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			if (userAuth) {
@@ -42,15 +43,18 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
-					<Route exact path="/signin" component={SignInAndSignUp} />
+					<Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)} />
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const mapsStateToProps = ({user}) => ({
+	currentUser: user.currentUser
+});
 const mapsDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapsDispatchToProps)(App);
+export default connect(mapsStateToProps, mapsDispatchToProps)(App);
