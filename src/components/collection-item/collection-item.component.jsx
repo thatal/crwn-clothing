6b9c131d382934687390c44b6525a281ce5a6196
Sunil Thatal <sunilthatal007@gmail.com>
2020-04-14 22:29:00 +0530

    card add item added. with payoad

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import "./collection-item.styles.scss";
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
     return (
         <div className="collection-item">
             <div className="image"
@@ -11,11 +15,16 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
             >
             </div>
             <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
             </div>
+            <CustomButton onClick={() => addItem(item)}>Add to cart</CustomButton>
         </div>
     );
 }
 
-export default CollectionItem;
+const mapsDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapsDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({title, items}) => {
             <div className="preview">
                 {
                     items.filter((item, idx) => idx < 4)
-                    .map(({id, ...otherCollectionItemProps}) => (
-                        <CollectionItem key={id} {...otherCollectionItemProps}></CollectionItem>
+                    .map((item) => (
+                        <CollectionItem key={item.id} item={item}></CollectionItem>
                     ))
                 }
             </div>
