ba512aefeef47c64590bf47bf2b7f354610aa738
Sunil Thatal <sunilthatal007@gmail.com>
2020-04-29 15:32:50 +0530

    style fixed

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,6 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_oI2FhlQOLkMw3vIcq3dhLqzO00b2jsABEX';
-    const secretKey = 'sk_test_lOJ5mem33SNxvR8M4DmdB2dX00a4ipJZBV';
     const onToken = token => {
         alert("Payment Successful")
     }
